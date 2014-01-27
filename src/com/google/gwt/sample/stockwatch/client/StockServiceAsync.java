6574fe33dba3243b3d257b3a7298aede0a75a0a0
Hui-hong Lin <hui811@gmail.com>
2014-01-26 23:22:10 -0800

    Added comments into StockServiceAsync

--- src/com/google/gwt/sample/stockwatch/client/StockServiceAsync.java
+++ src/com/google/gwt/sample/stockwatch/client/StockServiceAsync.java
@@ -2,8 +2,12 @@ package com.google.gwt.sample.stockwatch.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+// Pretend to change stuff
+
 public interface StockServiceAsync {
   public void addStock(String symbol, AsyncCallback<Void> async);
   public void removeStock(String symbol, AsyncCallback<Void> async);
+  
+// Changing stuff
   public void getStocks(AsyncCallback<String[]> async);
 }
